8b5c040f8395bd7eda8fa877f5dca4a6875e72f6
David Puerto Guerrero <dsp5502@gmail.com>
2022-05-17 13:04:56 -0500

    Orden Carpetas

---
+++ src/Firebase/fireBaseConfing.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC8ZcSqYNvNkFfRmM7Ge--wvg72e3qANrI',
+  authDomain: 'realmod-9ba50.firebaseapp.com',
+  projectId: 'realmod-9ba50',
+  storageBucket: 'realmod-9ba50.appspot.com',
+  messagingSenderId: '381396237282',
+  appId: '1:381396237282:web:465fb5bd3ce89bbc9397bb',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
